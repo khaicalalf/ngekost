c9688dd2757277cefe078938131e6a808d156451
Muhammad Ghufron Khaical Alfaris <alfaris.mgk@gmail.com>
2025-02-11 13:47:40 +0700

    update show category in home

--- app/Repositories/CategoryRepository.php
+++ app/Repositories/CategoryRepository.php
@@ -4,11 +4,17 @@ namespace App\Repositories;
 
 use App\Interfaces\CategoryRepositoryInterface;
 use App\Models\Category;
+use Illuminate\Database\Eloquent\Builder;
 
 class CategoryRepository implements CategoryRepositoryInterface
 {
     public function getAllCategories()
     {
         return Category::all();
+    }
+
+    public function getCategoryBySlug($slug)
+    {
+        return Category::where('slug', $slug)->first();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\BoardingHouseController;
 use App\Http\Controllers\BookingController;
+use App\Http\Controllers\CaategoryController;
 use App\Http\Controllers\HomeController;
 use App\Models\BoardingHouse;
 use Illuminate\Support\Facades\Route;
@@ -12,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 
 // Route::get('/', 'HomeController@index')->name('home');
 Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('/category/{slug}', [CaategoryController::class, 'show'])->name('category.show');
 
 Route::get('/find-kost', [BoardingHouseController::class, 'find'])->name('find-kost');
 Route::get('/find-results', [BoardingHouseController::class, 'findResults'])->name('find-kost.results');
